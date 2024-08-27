60951a0a917511265c74ec369dcfe117ee3f20aa
LDoming <luis.dominguez218540@potros.itson.edu.mx>
2024-08-26 19:27:13 -0700

    Primer commit

---
+++ src/main/java/com/mycompany/puntoventaabarrotes/PuntoVentaAbarrotes.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.puntoventaabarrotes;
+
+/**
+ *
+ * @author ldoar
+ */
+public class PuntoVentaAbarrotes {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
